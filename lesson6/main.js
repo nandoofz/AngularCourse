4c65c2d3a85afc2ab5332e2417086a9e07c11ee6
Paulo Fernando <nandooferraz@gmail.com>
2015-10-15 17:43:01 -0300

    Lesson6

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,26 +1,28 @@
-var app = angular.module('minmax', []);
+var app = angular.module('minmax', [
+	'jcs-autoValidate'
+]);
 
-
-
+app.run(function (defaultErrorMessageResolver) {
+		defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
+			errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+			errorMessages['tooOld'] = 'You must be max {0} years old to use this site';
+			errorMessages['badUsername'] = 'Username can only contain numbers and letters and _';
+		});
+	}
+);
 
 app.controller('MinMaxCtrl', function ($scope, $http) {
 	$scope.formModel = {};
 
-	$scope.onSubmit = function (valid) {
+	$scope.onSubmit = function () {
+		console.log("Hey i'm submitted!");
+		console.log($scope.formModel);
 
-		if (valid) {
-			console.log("Hey i'm submitted!");
-			console.log($scope.formModel);
-
-			//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
-			//	success(function (data) {
-			//		console.log(":)")
-			//	}).error(function(data) {
-			//		console.log(":(")
-			//	});
-		} else {
-			console.log("Invalid Form!")
-		}
-
-	};
+		//$http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel).
+		//	success(function (data) {
+		//		console.log(":)")
+		//	}).error(function(data) {
+		//		console.log(":(")
+		//	});
+};
 });
