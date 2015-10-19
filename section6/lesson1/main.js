0f6bc1d3d72ba7d57b29eec95e974baf4f60c929
Paulo Fernando <nandooferraz@gmail.com>
2015-10-19 15:32:16 -0200

    Section6 - Lesson1-10

--- section6/lesson1/main.js
+++ section6/lesson1/main.js
@@ -16,10 +16,18 @@ app.config(function ($httpProvider, $resourceProvider, laddaProvider) {
 
 app.controller('PersonDetailController', function ($scope, ContactService) {
 	$scope.contacts = ContactService;
+
+	$scope.save = function(){
+		$scope.contacts.updateContact($scope.contacts.selectedPerson);
+	};
 });
 
 app.factory('Contact', function ($resource) {
-	return $resource("https://codecraftpro.com/api/samples/v1/contact/:id/");
+	return $resource("https://codecraftpro.com/api/samples/v1/contact/:id/", {id: '@id'}, {
+		update: {
+			method: 'PUT'
+		}
+	});
 });
 
 app.controller('PersonListController', function ($scope, ContactService) {
@@ -56,6 +64,7 @@ app.service('ContactService', function (Contact) {
 		'page': 1,
 		'hasMore': true,
 		'isLoading': false,
+		'isSaving': false,
 		'selectedPerson': null,
 		'persons': [],
 		'search': null,
@@ -103,6 +112,12 @@ app.service('ContactService', function (Contact) {
 				self.page += 1;
 				self.loadContacts();
 			}
+		},
+		'updateContact': function (person){
+			self.isSaving = true;
+			person.$update().then(function (){
+				self.isSaving = false;
+			});
 		}
 	};
 
