d0e2470ce6faf50f93fdd6bc0a82b6884d80d80e
Paulo Fernando <nandooferraz@gmail.com>
2015-10-20 14:47:43 -0200

    Section9 - Lesson1-3

--- section9/lesson1/main.js
+++ section9/lesson1/main.js
@@ -78,8 +78,12 @@ app.factory("Contact", function ($resource) {
 
 app.directive('ccSpinner', function () {
 	return {
+		'transclude': true,
 		'restrict': 'E',
-		'templateUrl': 'templates/spinner.html'
+		'templateUrl': 'templates/spinner.html',
+		'scope': {
+			'isLoading': '='
+		}
 	};
 });
 
